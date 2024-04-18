033465a4b16d401ae4c05c82449245ff8b90af53
Marie-ange MBALA <marie-angembala@MacBook-Pro-de-Marie-ange.local>
2024-04-19 01:06:24 +0200

    navbar design

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,36 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css'; 
-// Importez une icône de votre choix, ici un exemple avec Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FaceIcon from '@mui/icons-material/Face';
 import { useAuth } from '../../context/authContext';
 import LoginModal from '../LoginModal';
 
 const Navbar = () => {
   const { isAdminLoggedIn, logout } = useAuth();
   const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+  const [isLogoutDialogOpen, setLogoutDialogOpen] = useState(false);
 
   const handleAuthClick = () => {
     if (isAdminLoggedIn) {
-      logout();
+      setLogoutDialogOpen(true); // Open logout confirmation dialog
     } else {
-      setLoginModalOpen(true); // Ouvre le modal si l'utilisateur n'est pas connecté
+      setLoginModalOpen(true); // Open login modal
     }
   };
-  useEffect(() => {}, [isAdminLoggedIn]);
+
+  const handleLogout = () => {
+    logout();
+    setLogoutDialogOpen(false);
+  };
 
   return (
     <>
       <nav className="navbar">
-      <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>About</NavLink>
-      <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink>
-      <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink>
+        <h1 className='name'>Marie-Ange MBALA</h1>
+        <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>About me</NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>My Projects</NavLink>
+        <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact-me</NavLink>
         <div className="navbar-login-icon" onClick={handleAuthClick}>
-          {isAdminLoggedIn ? <FontAwesomeIcon icon={faSignOutAlt} /> : <FontAwesomeIcon icon={faSignInAlt} />}
+          {isAdminLoggedIn ? 
+            <div className="icon-container">
+              <FaceIcon style={{ fontSize: 30 }} />
+              <div className="icon-label">Log-out</div>
+            </div> :
+            <div className="icon-container">
+              <AccountCircleIcon style={{ fontSize: 30 }} />
+              <div className="icon-label">Log-in</div>
+            </div>
+          }
         </div>
       </nav>
       <LoginModal isOpen={isLoginModalOpen} onRequestClose={() => setLoginModalOpen(false)} />
+
+      {/* Logout Confirmation Dialog */}
+      <Dialog open={isLogoutDialogOpen} onClose={() => setLogoutDialogOpen(false)}>
+        <DialogTitle>Logout Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Do you really want to log out?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setLogoutDialogOpen(false)}>Cancel</Button>
+          <Button onClick={handleLogout} color="primary" autoFocus>Yes</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
